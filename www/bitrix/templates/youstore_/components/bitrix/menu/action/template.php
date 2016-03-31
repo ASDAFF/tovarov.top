0d914ef6140636ecf95d08a4878220c920078af6
svoyn <svoynrav@yandex.ru>
2016-03-31 20:34:59 +0300

    Небольшие правки

--- www/bitrix/templates/youstore_/components/bitrix/menu/action/template.php
+++ www/bitrix/templates/youstore_/components/bitrix/menu/action/template.php
@@ -1,7 +1,7 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?$this->setFrameMode(true);?>
 <?if(!empty($arResult)):?>
-	<h3><?=GetMessage("TITLE")?></h3>
+	<p class="head-text"><?=GetMessage("TITLE")?></p>
 	<ul class="footer-menu">
 		<?foreach($arResult as $arItem):?>
 			<li>
