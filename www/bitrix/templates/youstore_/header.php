fb39111a2e6ec80e05fc4edafc3c4f6b6f530b98
svoyn <svoynrav@yandex.ru>
2016-03-31 11:33:36 +0300

    Favicon

--- www/bitrix/templates/youstore_/header.php
+++ www/bitrix/templates/youstore_/header.php
@@ -25,25 +25,25 @@ IncludeTemplateLangFile(__FILE__);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $APPLICATION->ShowTitle() ?> » Tovarov.TOP</title>
 
-
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"
-          href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= SITE_TEMPLATE_PATH ?>/images/favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="<?= SITE_TEMPLATE_PATH ?>/images/favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="<?= SITE_TEMPLATE_PATH ?>/images/favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="<?= SITE_TEMPLATE_PATH ?>/images/favicon/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="<?= SITE_TEMPLATE_PATH ?>/images/favicon/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="<?= SITE_TEMPLATE_PATH ?>/images/favicon/mstile-310x310.png" />
 
     <link
         href='https://fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic&subset=latin,cyrillic-ext,latin-ext,cyrillic'
